8123fb60a79d2434739a8383c7c6ff71c21997da
47hm4n3 <pixel@canalip70.canalip.upmc.fr>
2018-01-09 18:50:39 +0100

    Almost finished the application interface

--- app/src/main/java/com/pixel/dizainier/MainActivity.java
+++ app/src/main/java/com/pixel/dizainier/MainActivity.java
@@ -2,6 +2,9 @@ package com.pixel.dizainier;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar sb = findViewById(R.id.mySeeker);
+
     }
+
 }
